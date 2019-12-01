abe9430f0fd4e2b0c4160a1e1dbcc1be9745f9e9
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-01 19:26:21 +0000

    E300L_WW-user 8.0.0 OPR1.170623.032 23 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/asus/ZB555KL
+BOARD_VENDOR := asus
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/asus/ZB555KL/BoardConfigVendor.mk
---
+++ lineage_ZB555KL.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from ZB555KL device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := ZB555KL
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := lineage_ZB555KL
+PRODUCT_MODEL := ZB555KL
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := ZB555KL
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="E300L_WW-user 8.0.0 OPR1.170623.032 23 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := E300L_WW-user-8.0.0-OPR1.170623.032-23-release-keys
